6b099188c044800416c6e835aaa37baefd6e2c86
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-04-08 03:43:49 +0700

    First commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller {
+	
+	public function login(Request $request) {
+		$email = $request->input('email');
+		$password = $request->input('password');
+		$admins = DB::select("SELECT * FROM `admin` WHERE `email`='".$email."' AND `password`='".$password."'");
+		if (sizeof($admins) > 0) {
+			echo json_encode(array(
+				'response_code' => 0,
+				'data' => array(
+					'admin' => $admins[0]
+				)
+			));
+		} else {
+			echo json_encode(array(
+				'response_code' => -1
+			));
+		}
+	}
+	
+	public function get_apps(Request $request) {
+		$apps = DB::select("SELECT * FROM `app` ORDER BY `name`");
+		return array(
+			'response_code' => 0,
+			'data' => array(
+				'apps' => $apps
+			)
+		);
+	}
+	
+	public function search_app(Request $request) {
+		$query = $request->input('query');
+		$apps = DB::select("SELECT * FROM `app` WHERE `name` LIKE '%".$query."%' ORDER BY `name`");
+		return array(
+			'response_code' => 0,
+			'data' => array(
+				'apps' => $apps
+			)
+		);
+	}
+	
+	public function get_accounts(Request $request) {
+		$appID = intval($request->input('app_id'));
+		$accounts = DB::select("SELECT * FROM `account` WHERE `app_id`=".$appID);
+		return array(
+			'response_code' => 0,
+			'data' => array(
+				'accounts' => $accounts
+			)
+		);
+	}
+	
+	public function search_account(Request $request) {
+		$query = $request->input('query');
+		$accounts = DB::select("SELECT * FROM `account` WHERE `phone` LIKE '%".$query."%'");
+		return array(
+			'response_code' => 0,
+			'data' => array(
+				'accounts' => $accounts
+			)
+		);
+	}
+	
+	public function get_admins(Request $request) {
+		$admins = DB::select("SELECT * FROM `admin` ORDER BY `name`");
+		return array(
+			'response_code' => 0,
+			'data' => array(
+				'admins' => $admins
+			)
+		);
+	}
+	
+	public function search_admin(Request $request) {
+		$query = $request->input('query');
+		$admins = DB::select("SELECT * FROM `admin` WHERE `name` LIKE '%".$query."%' OR `email` LIKE '%".$query."%' ORDER BY `name`");
+		return array(
+			'response_code' => 0,
+			'data' => array(
+				'admins' => $admins
+			)
+		);
+	}
+	
+	public function add_admin(Request $request) {
+		$name = $request->input('name');
+		$email = $request->input('email');
+		$password = $request->input('password');
+		DB::insert("INSERT INTO `admin` (`name`, `email`, `password`) VALUES ('".$name."', '".$email."', '".$password."')");
+	}
+	
+	public function delete_admin(Request $request) {
+		$id = intval($request->input('id'));
+		DB::select("DELETE FROM `admin` WHERE `id`=".$id);
+	}
+	
+	public function update_admin(Request $request) {
+		$id = intval($request->input('id'));
+		$name = $request->input('name');
+		$password = $request->input('password');
+		DB::insert("UPDATE `admin` SET `name`='".$name."', `password`='".$password."' WHERE `id`=".$id);
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::post('/admin/login', [AdminController::class, 'login']);
+Route::get('/admin/get_apps', [AdminController::class, 'get_apps']);
+Route::post('/admin/search_app', [AdminController::class, 'search_app']);
+Route::post('/admin/get_accounts', [AdminController::class, 'get_accounts']);
+Route::post('/admin/search_account', [AdminController::class, 'search_account']);
+Route::get('/admin/get_admins', [AdminController::class, 'get_admins']);
+Route::post('/admin/search_admin', [AdminController::class, 'search_admin']);
+Route::post('/admin/add', [AdminController::class, 'add_admin']);
+Route::post('/admin/delete', [AdminController::class, 'delete_admin']);
+Route::post('/admin/update', [AdminController::class, 'update_admin']);
